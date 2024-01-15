55f82d9e7782266b209107bc0246d251a63cef25
Diego <dbandin@gmail.com>
2024-01-15 22:50:58 +0000

    Renamed tmux script and inventory files

---
+++ tmux-sessions/tmux_virtual_sessions.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+    echo "Missing argument"
+    echo "usage: ${0} inventory_file_name"
+    exit 1
+fi
+
+INVENTORY=$1
+
+echo "* Inventory file: ${1}"
+
+echo "  * Processing hosts"
+
+# Rough counter to identify first line, because the first line is expected to be the ansible controller
+line_counter=0
+split_list=()
+while read -a line; do
+    echo "    ** Processing line ${line_counter}"
+    echo "    * Name/Description: ${line[0]} - Port: ${line[1]} - Login: ${line[2]} - Host: ${line[3]}"
+
+    if [[ line_counter -eq 0 ]];then
+        session_command="ssh -p ${line[1]} -l ${line[2]} ${line[3]}"
+    else
+        split_list+=( split-pane ssh -l ${line[2]} -p ${line[1]} ${line[3]} ';' )
+    fi
+
+    let line_counter++
+done < $1
+
+# Un-comment the synchronize-panes option if you want to enable broadcasting the commands to all panes.
+tmux new-session "${session_command}" ';' \
+    "${split_list[@]}" \
+    select-layout tiled ';' \
+    # set-option -w synchronize-panes
+
+exit 0
